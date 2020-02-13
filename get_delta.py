80b5f31207a8e7ff93d4cc50ec33133af95f45c2
noname <rekhlov.lev@mail.ru>
2020-02-13 16:17:30 +0300

    Big task of Maps API. Part #1

---
+++ get_delta.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def get_spn(address):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": address,
+        "format": "json"}
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+    if not response:
+        print('Ошибка!')
+        exit(0)
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    toponym_upper_corner = list(map(float, toponym['boundedBy']['Envelope']['upperCorner'].split()))
+    toponym_lower_corner = list(map(float, toponym['boundedBy']['Envelope']['lowerCorner'].split()))
+    delta1 = str(abs(toponym_lower_corner[0] - toponym_upper_corner[0]))
+    delta2 = str(abs(toponym_lower_corner[1] - toponym_upper_corner[1]))
+    return ','.join([delta1, delta2])
--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import pygame
 import os
+import requests
+from PIL import Image
+import sys
+from io import BytesIO
+from get_delta import get_spn
 
-width, heigth = 450, 450
-screen = pygame.display.set_mode((width, heigth))
-clock = pygame.time.Clock()
-running = True
+
+def load_map(toponym_to_find):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        print('Ошибка!')
+        exit(0)
+
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"].split()
+    toponym_longitude, toponym_lattitude = toponym_coodrinates
+
+    delta = get_spn(toponym_to_find)
+
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": delta,
+        "l": "map",
+        "pt": f"{','.join(toponym_coodrinates)},pm2wtl",
+        'size': '450,450'
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    im = Image.open(BytesIO(response.content))
+    im.save('data/map.png')
 
 
 def load_image(name, colorkey=None):
@@ -19,7 +56,13 @@ def load_image(name, colorkey=None):
     return image
 
 
-image = load_image('biba.png')
+width, heigth = 450, 450
+screen = pygame.display.set_mode((width, heigth))
+clock = pygame.time.Clock()
+running = True
+address = " ".join(sys.argv[1:])
+load_map(address)
+image = load_image('map.png')
 
 while running:
     for event in pygame.event.get():
