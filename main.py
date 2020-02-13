aa0e43afbdbed7366920661de1a9d9ec7f9be0da
plov-cyber <rekhlov.lev@mail.ru>
2020-02-13 19:34:32 +0300

    Optimized scaling and loading of map.

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from io import BytesIO
 from get_delta import get_spn
 
 
-def load_map(toponym_to_find):
+def initialize():
+    global address, delta, coordinates, image
+    address = " ".join(sys.argv[1:])
+    delta = get_spn(address)
+
     geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
     geocoder_params = {
         "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": toponym_to_find,
+        "geocode": address,
         "format": "json"}
 
     response = requests.get(geocoder_api_server, params=geocoder_params)
@@ -24,14 +28,14 @@ def load_map(toponym_to_find):
     json_response = response.json()
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
-    toponym_coodrinates = toponym["Point"]["pos"].split()
-    toponym_longitude, toponym_lattitude = toponym_coodrinates
+
+    coordinates = [float(i) for i in toponym["Point"]["pos"].split()]
 
     map_params = {
-        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "ll": ",".join([str(i) for i in coordinates]),
         "spn": f'{delta:.3f},{delta:.3f}',
         "l": "map",
-        "pt": f"{','.join(toponym_coodrinates)},pm2wtl",
+        "pt": f"{','.join([str(i) for i in coordinates])},pm2wtl",
         'size': '450,450'
     }
 
@@ -44,6 +48,29 @@ def load_map(toponym_to_find):
 
     im = Image.open(BytesIO(response.content))
     im.save('map.png')
+    image = load_image('map.png')
+
+
+def load_map():
+    global image
+    map_params = {
+        "ll": ",".join([str(i) for i in coordinates]),
+        "spn": f'{delta:.3f},{delta:.3f}',
+        "l": "map",
+        "pt": f"{','.join([str(i) for i in coordinates])},pm2wtl",
+        'size': '450,450'
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    if not response:
+        print('Ошибка!')
+        exit(0)
+
+    im = Image.open(BytesIO(response.content))
+    im.save('map.png')
+    image = load_image('map.png')
 
 
 def load_image(name, colorkey=None):
@@ -58,35 +85,30 @@ def load_image(name, colorkey=None):
     return image
 
 
-def update_map():
-    global image
-    load_map(address)
-    image = load_image('map.png')
-
-
-width, heigth = 450, 450
-screen = pygame.display.set_mode((width, heigth))
+width, height = 450, 450
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
-address = " ".join(sys.argv[1:])
-delta = get_spn(address)
-load_map(address)
-image = load_image('map.png')
+image = None
+address = None
+delta = None
+coordinates = None
+initialize()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_PAGEUP]:
-            if delta > 0.001:
-                delta *= 0.5
-            update_map()
-        elif keys[pygame.K_PAGEDOWN]:
-            if delta < 10:
-                delta *= 1.5
-            update_map()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_PAGEUP:
+                if delta > 0.002:
+                    delta *= 0.5
+            elif event.key == pygame.K_PAGEDOWN:
+                if delta < 10:
+                    delta *= 1.5
+
+    load_map()
     screen.blit(image, (0, 0))
     pygame.display.flip()
-    clock.tick(15)
+    clock.tick(60)
 pygame.quit()
