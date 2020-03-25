734e416678338d64e473a72bfa923dc3ac0f953a
plov-cyber <rekhlov.lev@mail.ru>
2020-03-25 17:20:14 +0300

    Big task of Maps API. Part #5-11

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ class Map(QMainWindow):
         self.geo_server = "http://geocode-maps.yandex.ru/1.x/"
         self.map_server = "http://static-maps.yandex.ru/1.x/"
         self.delta = None
+        self.full_address = ''
+        self.postal_code = ''
         self.coordinates = None
         self.geo_params = None
         self.map_params = None
+        self.mark = None
 
         self.scheme.setChecked(True)
         self.map_type.setId(self.scheme, 2)
@@ -29,30 +32,41 @@ class Map(QMainWindow):
         self.map_type.setId(self.hybrid, 0)
         self.map_type.buttonClicked[int].connect(self.change_map_type)
 
-        self.map_init()
+        self.search.clicked.connect(self.search_object)
+        self.delete_mark.clicked.connect(self.delete_object)
+        self.postalcode.stateChanged[int].connect(self.postal_code_view)
 
-    def map_init(self):
-        self.delta = get_spn(self.address)
-        self.geo_params = {
-            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-            "geocode": self.address,
-            "format": "json"
-        }
+        self.load_map(new_address=True)
 
-        response = requests.get(self.geo_server, params=self.geo_params)
-        if not response:
-            print('Ошибка при нахождении заданного адреса!')
-            exit(0)
+    def load_map(self, new_address=False):
+        if new_address:
+            self.delta = get_spn(self.address)
+            self.geo_params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                "geocode": self.address,
+                "format": "json"
+            }
 
-        json_response = response.json()
-        self.coordinates = list(map(float, json_response["response"]["GeoObjectCollection"][
-            "featureMember"][0]["GeoObject"]['Point']['pos'].split()))
+            response = requests.get(self.geo_server, params=self.geo_params)
+            if not response:
+                print('Ошибка при нахождении заданного адреса!')
+                exit(0)
+
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"][
+                "featureMember"][0]["GeoObject"]
+            self.coordinates = list(map(float, toponym['Point']['pos'].split()))
+            self.mark = ",".join(list(map(str, self.coordinates))) + ',flag'
+            self.full_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+            if 'postal_code' in toponym["metaDataProperty"]["GeocoderMetaData"]['Address']:
+                self.postal_code = toponym["metaDataProperty"]["GeocoderMetaData"]['Address']["postal_code"]
+            self.postal_code_view(self.postalcode.isChecked())
 
         self.map_params = {
             "ll": ",".join(list(map(str, self.coordinates))),
             "spn": f'{self.delta:.3f},{self.delta:.3f}',
-            "l": "map",
-            # "pt": f"{','.join([str(i) for i in coordinates])},pm2wtl",
+            "l": self.map_types[self.now_type],
+            'pt': self.mark,
             'size': '450,450'
         }
         response = requests.get(self.map_server, params=self.map_params)
@@ -63,8 +77,28 @@ class Map(QMainWindow):
         self.pixmap.loadFromData(response.content)
         self.image.setPixmap(self.pixmap)
 
+    def postal_code_view(self, state):
+        if state == 0:
+            self.lbl_address.setText('Адрес: ' + self.full_address)
+        else:
+            self.lbl_address.setText('Адрес: ' + self.full_address + ' ' + self.postal_code)
+
     def change_map_type(self, id):
         self.now_type = id
+        self.load_map()
+
+    def search_object(self):
+        new_address = self.obj_address.text()
+        self.obj_address.clear()
+        if new_address:
+            self.address = new_address
+            self.load_map(new_address=True)
+
+    def delete_object(self):
+        self.mark = None
+        self.lbl_address.setText('Адрес: ')
+        self.full_address = ''
+        self.postal_code = ''
         self.load_map()
 
     def keyPressEvent(self, event):
@@ -74,32 +108,38 @@ class Map(QMainWindow):
         if event.key() == Qt.Key_PageDown:
             if self.delta < 10:
                 self.delta *= 1.5
-        if event.key() == Qt.Key_Up and -80 < self.coordinates[1] - self.delta:
-            self.coordinates[1] -= self.delta
+        if event.key() == Qt.Key_Down and -80 < self.coordinates[1] - self.delta:
+            self.coordinates[1] -= self.delta / 2
         if event.key() == Qt.Key_Up and self.coordinates[1] + self.delta < 80:
-            self.coordinates[1] += self.delta
+            self.coordinates[1] += self.delta / 2
         if event.key() == Qt.Key_Left and -180 < self.coordinates[0] - self.delta:
             self.coordinates[0] -= self.delta
         if event.key() == Qt.Key_Right and self.coordinates[0] + self.delta < 180:
             self.coordinates[0] += self.delta
         self.load_map()
 
-    def load_map(self):
-        self.map_params = {
-            "ll": ",".join([str(i) for i in self.coordinates]),
-            "spn": f'{self.delta:.3f},{self.delta:.3f}',
-            "l": self.map_types[self.now_type],
-            # "pt": f"{','.join([str(i) for i in coordinates])},pm2wtl",
-            'size': '450,450'
-        }
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.coordinates[0] += ((event.x() - 225.5) / 225.5) * (self.delta / 2)
+            self.coordinates[1] += ((225.5 - event.y()) / 225.5) * (self.delta / 2)
+        self.find_address()
+        self.load_map(new_address=True)
 
-        response = requests.get(self.map_server, params=self.map_params)
+    def find_address(self):
+        self.geo_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": ",".join(list(map(str, self.coordinates))),
+            "format": "json"
+        }
+        response = requests.get(self.geo_server, params=self.geo_params)
         if not response:
-            print('Ошибка при запросе изображения!')
+            print('Ошибка при нахождении заданного адреса!')
             exit(0)
 
-        self.pixmap.loadFromData(response.content)
-        self.image.setPixmap(self.pixmap)
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        self.address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
 
 
 if __name__ == "__main__":
